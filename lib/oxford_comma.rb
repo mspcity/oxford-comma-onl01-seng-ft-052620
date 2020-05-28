1cdc6a81c40be732d7b7ce60c01b1a0abf3a11f9
Youngmin Park <codepark2019@gmail.com>
2020-05-28 23:55:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+      # binding.pry
+      array[-1] = "and " + array[-1]
+      array.join(", ")
+  end
 end
