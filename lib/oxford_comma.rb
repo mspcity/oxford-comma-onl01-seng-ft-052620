22b50155a1803d2526dd89eec6f8de7d3dd70e89
Youngmin Park <codepark2019@gmail.com>
2020-05-28 19:59:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def oxford_comma(array)
+
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+      binding.pry
+  end
 
 end
